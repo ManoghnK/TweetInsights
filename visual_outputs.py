29d9c66facb4bdd87fded1a08b31c9ffd6dedf8e
Krunal Murlidhar Kumbhare <krunalk013@gmail.com>
2021-11-15 18:36:59 +0530

    Added group bar chart

--- visual_outputs.py
+++ visual_outputs.py
@@ -1,8 +1,62 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+
+def format_data_for_grouped_barchart(filename):
+    
+    groupwise_sentiments = {}
+    # Syntax: group => [positive,negative,neutral]
+    groupwise_sentiments['Health'] = [0,0,0]
+    groupwise_sentiments['Economy'] = [0,0,0]
+    groupwise_sentiments['Culture'] = [0,0,0]
+    groupwise_sentiments['Social'] = [0,0,0]
+    
+    with open(filename, 'r') as csvfile:
+        lines = csv.reader(csvfile, delimiter = ',')
+        for row in lines:
+            if row[12] in ['Health', 'Economy', 'Culture', 'Social']:
+                if row[11] == 'negative':
+                    groupwise_sentiments[row[12]][1] += 1
+                elif row[11] == 'positive':
+                    groupwise_sentiments[row[12]][0] += 1
+                else:
+                    groupwise_sentiments[row[12]][2] += 1
+                    
+    return groupwise_sentiments
+    
+    
+
+def grouped_barchart( filename):
+    data = format_data_for_grouped_barchart( filename)
+    x = np.arange(4)
+    # y1 = [34, 56, 12, 89, 67]
+    # y2 = [12, 56, 78, 45, 90]
+    # y3 = [14, 23, 45, 25, 89]
+    y1 = []
+    y2 = []
+    y3 = []
+    
+    for key in data:
+        y1.append(data[key][0])
+        y2.append(data[key][1])
+        y3.append(data[key][2])
+    
+    width = 0.2
+
+    # plot data in grouped manner of bar type
+    plt.bar(x-0.2, y1, width, color='cyan')
+    plt.bar(x, y2, width, color='orange')
+    plt.bar(x+0.2, y3, width, color='green')
+    # plt.xticks(x, ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'])
+    plt.xticks(x, data.keys())
+    plt.xlabel("Tweet categories")
+    plt.ylabel("Tweet counts")
+    plt.legend(['Positive tweets', 'Negative tweets', 'Neutral tweets'])
+    plt.show()
+
 
 def visualise_pie(filename):
-    X = ['Positive', 'Negative', 'Neutral']
+    X = ['Positive tweets', 'Negative tweets', 'Neutral tweets']
     Y = []
     neutral_count = 0
     positive_count = 0
@@ -25,7 +79,7 @@ def visualise_pie(filename):
     Y.append(neutral_count)
     
     plt.pie(Y,labels = X,autopct = '%.2f%%')
-    plt.title('Sentiments of users', fontsize = 20)
+    plt.title('Sentiments analysis of tweets', fontsize = 20)
     plt.figure(1)
     plt.show()
 
@@ -58,3 +112,4 @@ def visualise_bar(filename):
 
 visualise_pie('sentimentanalysis.csv')
 visualise_bar('clusterclassification.csv')
+grouped_barchart('sentiment_and_cluster.csv')
